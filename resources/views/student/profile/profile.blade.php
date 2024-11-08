762736d7c4346be25980483bdf1d7ef755f4bfd3
rzgonzaga <rzgonzaga@usm.edu.ph>
2024-11-08 18:01:58 +0800

    handle School Not Found

--- resources/views/student/profile/profile.blade.php
+++ resources/views/student/profile/profile.blade.php
@@ -166,7 +166,7 @@ USMCEE - My Profile
                         <div class="xl:col-span-3">
                             <label for="choices-single-default" class="inline-block mb-2 text-base font-medium">School
                                 ID <sup class="text-red-500">* required</sup></label>
-                            <input type="number" id="school_id" name="school_id"
+                            <input type="text" id="school_id" name="school_id"
                                 class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200"
                                 placeholder="School ID" value="{{ $studentdetails->schoolid }}"
                                 onchange="handleChange(this.value)">
