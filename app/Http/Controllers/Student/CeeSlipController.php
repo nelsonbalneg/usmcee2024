214e42e8b068ac862e6e7bc949c699a79b441a34
nelsonbalneg <nelsonbalneg@gmail.com>
2024-11-11 09:03:45 +0800

    Added Print date in cee slip

--- app/Http/Controllers/Student/CeeSlipController.php
+++ app/Http/Controllers/Student/CeeSlipController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Student;
 
 use PDF;
+use Carbon\Carbon;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
--- resources/views/student/cee-slip/exam-slip.blade.php
+++ resources/views/student/cee-slip/exam-slip.blade.php
@@ -9,133 +9,139 @@ New Report Machine Ledger
     <title>CEE Examination Slip </title>
     <style>
         @page {
-    size: 8.5in 13in;
-    margin: 0.25in;
-}
+            size: 8.5in 13in;
+            margin: 0.25in;
+        }
 
-body {
-    font-family: 'Corbel', sans-serif;
-    margin: 0;
-    padding: 0;
-    width: calc(8.5in - 0.5in);
-    height: 12.5in; /* Slightly smaller to avoid overflow */
-    box-sizing: border-box;
-    font-size: 9pt;
-}
+        body {
+            font-family: 'Corbel', sans-serif;
+            margin: 0;
+            padding: 0;
+            width: calc(8.5in - 0.5in);
+            height: 12.5in;
+            /* Slightly smaller to avoid overflow */
+            box-sizing: border-box;
+            font-size: 9pt;
+        }
 
-.container {
-    width: 100%;
-    height: 100%;
-    padding: 0; /* Remove padding */
-    box-sizing: border-box;
-    page-break-inside: avoid; /* Avoid page break inside */
-}
+        .container {
+            width: 100%;
+            height: 100%;
+            padding: 0;
+            /* Remove padding */
+            box-sizing: border-box;
+            page-break-inside: avoid;
+            /* Avoid page break inside */
+        }
 
-.header,
-.section {
-    width: 100%;
-    margin-bottom: 5px;
-    page-break-inside: avoid; /* Avoid page break inside */
-}
+        .header,
+        .section {
+            width: 100%;
+            margin-bottom: 5px;
+            page-break-inside: avoid;
+            /* Avoid page break inside */
+        }
 
-.section div {
-    font-size: 9pt;
-}
+        .section div {
+            font-size: 9pt;
+        }
 
-.header div {
-    text-align: center;
-    font-weight: bold;
-}
+        .header div {
+            text-align: center;
+            font-weight: bold;
+        }
 
-.header img {
-    position: absolute;
-    top: 0;
-    width: 100px;
-}
+        .header img {
+            position: absolute;
+            top: 0;
+            width: 100px;
+        }
 
-.header .left-logo {
-    left: 0;
-    margin-left: 100px;
-}
+        .header .left-logo {
+            left: 0;
+            margin-left: 100px;
+        }
 
-table {
-    width: 100%;
-    border-collapse: collapse;
-    page-break-inside: avoid; /* Prevent tables from breaking across pages */
-}
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            page-break-inside: avoid;
+            /* Prevent tables from breaking across pages */
+        }
 
-th,
-td {
-    border: 1px solid #000;
-    padding: 8px;
-    text-align: left;
-    font-size: 12px;
-    vertical-align: middle;
-}
+        th,
+        td {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: left;
+            font-size: 12px;
+            vertical-align: middle;
+        }
 
-th {
-    background-color: #cecece;
-    text-align: center;
-    white-space: nowrap;
-}
+        th {
+            background-color: #cecece;
+            text-align: center;
+            white-space: nowrap;
+        }
 
-.fixed-width th {
-    width: 130px;
-    height: 8px;
-}
+        .fixed-width th {
+            width: 130px;
+            height: 8px;
+        }
 
-.equipment-specifications th,
-.equipment-specifications td {
-    background-color: #a1a1a1;
-}
+        .equipment-specifications th,
+        .equipment-specifications td {
+            background-color: #a1a1a1;
+        }
 
-.round-image {
-    border-radius: 50%;
-    width: 200px;
-    height: 200px;
-    object-fit: cover;
-}
+        .round-image {
+            border-radius: 50%;
+            width: 200px;
+            height: 200px;
+            object-fit: cover;
+        }
 
-.watermark {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%) rotate(-45deg);
-    /* font-size: 100px; */
-    color: rgba(0, 0, 0, 0.1);
-    white-space: nowrap;
-    z-index: -1;
-    pointer-events: none;
-    opacity: 0.3; /* 20% opacity */
-}
+        .watermark {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) rotate(-45deg);
+            /* font-size: 100px; */
+            color: rgba(0, 0, 0, 0.1);
+            white-space: nowrap;
+            z-index: -1;
+            pointer-events: none;
+            opacity: 0.3;
+            /* 20% opacity */
+        }
 
-.title {
-    text-align: center;
-    margin: 5px 0;
-}
+        .title {
+            text-align: center;
+            margin: 5px 0;
+        }
 
-.row-height td {
-    height: 12px;
-}
+        .row-height td {
+            height: 12px;
+        }
 
-.text-left {
-    display: flex;
-    justify-content: space-between;
-}
+        .text-left {
+            display: flex;
+            justify-content: space-between;
+        }
 
-.info-item {
-    margin-right: 40px;
-}
+        .info-item {
+            margin-right: 40px;
+        }
 
-.footer {
-    position: absolute; /* Change to absolute instead of fixed */
-    bottom: 0;
-    width: 100%;
-    color: rgb(46, 46, 46);
-    font-size: 7pt;
-}
-
-        </style>
+        .footer {
+            position: absolute;
+            /* Change to absolute instead of fixed */
+            bottom: 0;
+            width: 100%;
+            color: rgb(46, 46, 46);
+            font-size: 7pt;
+        }
+    </style>
 </head>
 
 <body>
@@ -157,7 +163,8 @@ th {
         <div class="row d-flex justify-content-center" style="margin-bottom: 10px; margin-top:40px;">
             <div class="col">
                 <div class="text-left">
-                    <span class="info-item"><b>Congratulations!</b>You have successfully reserved a slot for the USMCEE. Below are your reservation details:</span>
+                    <span class="info-item"><b>Congratulations!</b>You have successfully reserved a slot for the USMCEE.
+                        Below are your reservation details:</span>
                 </div>
 
             </div>
@@ -172,7 +179,6 @@ th {
                 <td style="vertical-align: top; width: 50%;">
                     <table style="width: 100%;">
                         @if ($cee_reservation->photo)
-
                         @endif
                         <img src="{{ public_path($cee_reservation->applicant->photo) }}" alt="Applicant Photo"
                             style="width: 100px; height: 100px; margin-left: 120px;" class="round-image">
@@ -188,17 +194,18 @@ th {
                 <td style="vertical-align: top; width: 50%;">
                     <table style="width: 100%;">
                         <tr>
-                            <th style="text-align: left; width: 100px;">Test Session:</th>
+                            <th style="text-align: left; width: 100px;">Batch:</th>
                             <td>{{ $cee_reservation->exam_session }}</td>
                         </tr>
                         <tr>
                             <th style="text-align: left; width: 100px;">Test Venue:</th>
-                            <td> ({{ $cee_reservation->room->campus }}) {{ $cee_reservation->room->college_name }} - {{ $cee_reservation->room->room_name }}
+                            <td> <b>{{ $cee_reservation->room->campus }}</b><br> {{ $cee_reservation->room->college_name }} -
+                                {{ $cee_reservation->room->room_name }}
                             </td>
                         </tr>
                         <tr>
                             <th style="text-align: left; width: 100px;">Date and Time:</th>
-                            <td> {{ \Carbon\Carbon::parse($cee_reservation->room->schedule)->format('Y-m-d') }} -
+                            <td> {{ \Carbon\Carbon::parse($cee_reservation->room->schedule)->format('F j, Y') }}<br>
                                 {{ $cee_reservation->room->time }}</td>
                         </tr>
                         <tr>
@@ -230,22 +237,28 @@ th {
                         <li>Personal ballpen (not sign pen)</li>
                         <li>Pencil and sharpener</li>
                         <li>Snacks</li>
-                        </ul>
+                    </ul>
                 </td>
             </tr>
         </table>
 
+        @php
+            $app_no = $cee_reservation->app_no;
+        @endphp
+
         <div class="watermark">
             @for ($i = 0; $i < 55; $i++)
-            <p>USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 {{$cee_reservation->app_no}} USMCEE -2024 USMCEE -2024</p>
-        @endfor
-
-        <br>
-        {{-- <div class="footer">
-            <p>Downloaded Date and Time: {{ \Carbon\Carbon::now()->format('Y-m-d H:i:s') }}</p>
-            <p> <i>University of Southern Mindanao - College Entrance Examination Reservation System v4.0 | <b> Powered by: UICTO</b></i></p>
-        </div> --}}
-    </div>
+                <p>USMCEE -2024 {{ $app_no }} USMCEE -2024 {{ $app_no }} USMCEE -2024 {{ $app_no }}
+                    USMCEE -2024 {{ $app_no }} USMCEE -2024 {{ $app_no }} USMCEE -2024
+                    {{ $app_no }}
+                    USMCEE -2024 {{ $app_no }} USMCEE -2024 {{ $app_no }} USMCEE -2024 USMCEE -2024</p>
+            @endfor
+        </div>
+        <div class="footer">
+            <p>Downloaded Date and Time: {{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
+            <p style="margin-top:-8px;"> <i>University of Southern Mindanao - College Entrance Examination System v4.0 |
+                    <b> Powered by: UICTO</b></i></p>
+        </div>
 
 </body>
 
