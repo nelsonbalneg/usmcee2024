dd0eb8ee8a7aaff39d64c35e8bcc1d228df2ac66
nelsonbalneg <nelsonbalneg@gmail.com>
2025-04-23 13:22:48 +0800

    web php fix

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,17 @@ use App\Http\Controllers\Student\StudentCeeReserveController;
 
 
 
+
+// Route::get('/', function () {
+//     return view('auth.login');
+// })->middleware(['guest', DetectWebView::class]);
+
+// Route::middleware(['guest', DetectWebView::class, 'check.maintenance'])->group(function () {
+//     Route::get('/', function () {
+//         return view('auth.login');
+//     });
+// });
+
 Route::middleware(['guest', DetectWebView::class, 'check.maintenance'])->group(function () {
     Route::get('/', [AuthenticatedSessionController::class, 'create']);
 });
@@ -19,7 +30,7 @@ Route::middleware(['guest', DetectWebView::class, 'check.maintenance'])->group(f
 
 Route::middleware(['auth', 'verified', 'check.maintenance'])->group(function () {
     Route::get('/dashboard', function () {
-        $user = Auth::user()->id;
+        $user = auth()->user();
         if ($user) {
             // Check the user's role and redirect accordingly
             return match ($user->role) {
